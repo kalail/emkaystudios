111a9f450f87ee33442eafb81a02850f9ed7eacc
kalail <kashif610@gmail.com>
2012-08-06 18:16:48 +0500

    Add twitter stream.

--- emkaystudios/settings.py
+++ emkaystudios/settings.py
@@ -203,3 +203,5 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.contrib.messages.context_processors.messages",
 )
+
+TWITTER_USERNAME = "EmKayStudios"
--- emkaystudios/views.py
+++ emkaystudios/views.py
@@ -4,11 +4,15 @@ from emkaystudios.models import Announcement
 from blog.models import Post
 from itertools import chain
 from operator import attrgetter
+from emkaystudios.settings import TWITTER_USERNAME
+import twitter
 
 def index(request):
 	latest_announcement = Announcement.objects.latest('created_on')
 	latest_announcements = Announcement.objects.all().order_by('-created_on')[1:4]
 	latest_blog_posts = Post.objects.all().order_by('-created_on')[:3]
+
+	latest_tweets = twitter.Api().GetUserTimeline(TWITTER_USERNAME)[:5]
 
 	latest_items = sorted(
 		chain(latest_announcements, latest_blog_posts),
@@ -18,7 +22,7 @@ def index(request):
 	latest_items = latest_items[:3]
 
 	
-	return render_to_response('index.html', {'latest_announcement': latest_announcement, 'latest_items': latest_items }, context_instance=RequestContext(request))
+	return render_to_response('index.html', {'latest_announcement': latest_announcement, 'latest_items': latest_items, 'latest_tweets': latest_tweets }, context_instance=RequestContext(request))
 
 
 def about(request):
